bbfb6ed02cea1bdf2e286ce43792bc01af5d1ea7
Bors1k <8916misha@gmail.com>
2022-01-17 14:38:29 +0300

    update views, urls, settings

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,13 +1,31 @@
-from re import I
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .products import products
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('hello', safe=False)
+    routes = [
 
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
 def getProducts(request):
-    return JsonResponse(products,safe=False)
+    return Response(products)
+
+
+@api_view(['GET'])
+def getProduct(request,pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    
+    return Response(product)
