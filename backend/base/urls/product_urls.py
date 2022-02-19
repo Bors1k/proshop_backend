6087a40ed07df9c71b41b7071c012db1852ee003
Bors1k <8916misha@gmail.com>
2022-02-19 22:37:47 +0300

    add top of products view

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from base.views import product_view as views
 
 urlpatterns = [
-    path('',views.getProducts,name='products'),
-    path('create/',views.createProduct,name='product_create'),
-    path('upload_image/',views.uploadImage,name='product_uload_img'),
-    path('<str:pk>/',views.getProduct,name='product'),
-    path('<str:pk>/reviews/create/',views.createProductReview,name='create_review'),
-    path('<str:pk>/delete/',views.delProductById,name='product_delete'),
-    path('<str:pk>/update/',views.updateProduct,name='product_update'),
+    path('', views.getProducts, name='products'),
+    path('create/', views.createProduct, name='product_create'),
+    path('upload_image/', views.uploadImage, name='product_uload_img'),
+    path('top/', views.getTopProducts, name='top_products'),
+    path('<str:pk>/', views.getProduct, name='product'),
+    path('<str:pk>/reviews/create/',
+         views.createProductReview, name='create_review'),
+    path('<str:pk>/delete/', views.delProductById, name='product_delete'),
+    path('<str:pk>/update/', views.updateProduct, name='product_update'),
 ]
